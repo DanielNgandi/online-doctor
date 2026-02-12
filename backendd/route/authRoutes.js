04824241a380f92d7a65a687c47dad292b5e232e
DanielNgandi <danngandi1999@gmail.com>
2026-02-12 13:57:57 +0300

    signup

--- backendd/route/authRoutes.js
+++ backendd/route/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { login, register, getMe, registerDoctor } from "../controller/authController.js";
+import { upload } from "../middlewares/Mutter.js"; 
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/register/doctor", registerDoctor);
+router.post("/register", upload.single("photo"), register);
+router.post("/register/doctor", upload.single("photo"), registerDoctor);
 router.post("/login", login);
 router.get("/me",  getMe)
 
